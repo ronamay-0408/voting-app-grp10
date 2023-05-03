7ba3645ae915ab3dcf8f92bb5e76dfd503b8943b
Rona May Balangat <ronamay.balangat@bicol-u.edu.ph>
2023-05-03 23:26:01 +0800

    Edited

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 // const PORT = process.end.PORT || 8000;
 
-const defURL = "https://voting-app-grp10.onrender.com"
+const defURL = "http://localhost:8000/"
 
 app.use(express.static('public'));
 
